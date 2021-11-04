3060211b435e50846559957a9cfdad9ae119308f
Laurentiu <lcostin40@gmail.com>
2021-11-04 13:19:16 +0100

    Corretto le date

--- Milestone 3/JS/main.js
+++ Milestone 3/JS/main.js
@@ -141,7 +141,7 @@ const app = new Vue({
 
             var today = new Date();
 
-            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
 
             var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
